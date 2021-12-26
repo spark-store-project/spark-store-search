0665c4b93db03daf545b322a2b768632ced5b2f4
jerry <jerryww@aliyun.com>
2021-12-27 01:14:08 +0800

    fix: modify status code

--- api_server/src/api/appinfo.py
+++ api_server/src/api/appinfo.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
 
-import crud, schemas, models
+import crud
+import schemas
+import models
 
 router = APIRouter()
 
+
 @router.get("/appinfo/search", response_model=List[schemas.AppItem])
-def search(keyword: str, db: Session = Depends(models.get_db)):
+def search(keyword: str, res: Response, db: Session = Depends(models.get_db)):
     """
     根据关键字搜索应用
     """
@@ -19,7 +22,9 @@ def search(keyword: str, db: Session = Depends(models.get_db)):
         raise HTTPException(status_code=418, detail="搜索关键字不能为空！")
 
     apps = crud.search_app(db, keyword=keyword)
+    res.status_code = 200
     return apps
+
 
 @router.get("/appinfo/name", response_model=schemas.AppItem)
 def appinfo(pkgname: str, db: Session = Depends(models.get_db)):
@@ -32,13 +37,14 @@ def appinfo(pkgname: str, db: Session = Depends(models.get_db)):
     app = crud.get_app_item(db, pkgname=pkgname)
     return app
 
+
 @router.get("/appinfo/list", response_model=List[schemas.AppItem])
 def applist(page: int = 1, limit: int = 20, cate_name: str = "", db: Session = Depends(models.get_db)):
     """
     获取应用列表，默认查询所有应用，传入分类名称时，查询指定分类
     """
     cate_id = 0
-    
+
     if page < 1:
         page = 1
 
